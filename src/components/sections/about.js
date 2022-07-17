40df02f266e405d1f225499761faab4f1e112f09
ali <ali1995ez@gmail.com>
2022-07-17 11:45:24 +0430

    fix images

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -151,7 +151,7 @@ const About = () => {
           <div className="wrapper">
             <StaticImage
               className="img"
-              src="../../images/me.jpg"
+              src="../../images/me.webp"
               width={500}
               quality={95}
               formats={['AUTO', 'WEBP', 'AVIF']}
